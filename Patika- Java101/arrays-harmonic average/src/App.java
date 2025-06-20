7a68f86cdd37cafbc08aa6dc3dfb4bba6d093dad
Yunus Teke <yunusteke4646@gmail.com>
2025-06-21 00:56:09 +0300

    init

---
+++ Patika- Java101/arrays-harmonic average/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        double sum = 0.0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            sum += 1.0 / numbers[i];
+        }
+
+        double harmonicMean = numbers.length / sum;
+        System.out.println("Harmonik Ortalama: " + harmonicMean);
+    }
+}
