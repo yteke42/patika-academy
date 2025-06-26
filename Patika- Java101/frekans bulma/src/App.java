041bb8123abd6b5da28ba7ce45a952feb5c98883
Yunusteke <yunusteke4646@gmail.com>
2025-06-26 18:23:16 +0300

    init

--- Patika- Java101/frekans bulma/src/App.java
+++ Patika- Java101/frekans bulma/src/App.java
@@ -1,7 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        
-        int[] myArray =  new int[8];
-        System.out.print("");
+        System.out.print("Dizi : [10, 20, 20, 10, 10, 20, 5, 20]\n");
+        System.out.println("Tekrar Sayıları");
+
+        int[] myArray = {10, 20, 20, 10, 10, 20, 5, 20};
+        boolean[] visited = new boolean[myArray.length]; // Aynı eleman tekrar sayılmasın 
+
+        for (int i = 0; i < myArray.length; i++) {
+            if (visited[i]) continue; // Daha önce sayıldıysa atla
+
+            int count = 1; // En az 1 kere geçmiştir
+            for (int j = i + 1; j < myArray.length; j++) {
+                if (myArray[i] == myArray[j]) {
+                    count++;
+                    visited[j] = true;
+                }
+            }
+            System.out.println(myArray[i] + " sayısı " + count + " kere tekrar edildi.");
+        }
     }
 }
